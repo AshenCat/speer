90801a9bba7a1f83ed8456303ecf91d45431d4df
ashencat <forddagujar@gmail.com>
2021-08-15 23:19:27 -0400

    oopsie

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -7,39 +7,39 @@ import Stars from '../../assets/images/5_star.png'
 
 function Reviews() {
     return (
-        <>
-        <Nav 
+        <div style={{position: 'relative'}}>
+            <Nav 
             titleClassName="review-nav-title"
             bgClassName="review-nav-bg"
             liClassName="review-nav-li"
             svgClassName="review-nav-svg" />
-        <div className="reviews-container">
-            <div className="reviews-speakers">
-                <img src={Speaker1} alt="aa" className="reviews-speakers-img rsil"/>
-                <img src={Speaker2} alt="aa" className="reviews-speakers-img rsir"/>
-            </div>
-            <div className="reviews-text">
-                <h1 className="reviews-h">REVIEWS</h1>
-                <div className="reviews-boxes">
-                    <div className="reviews-box">
-                        <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
-                        <h2 className="reviews-artist">Artist</h2>
-                        <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
-                    </div>
-                    <div className="reviews-box">
-                        <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
-                        <h2 className="reviews-artist">Producer</h2>
-                        <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
-                    </div>
-                    <div className="reviews-box">
-                        <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
-                        <h2 className="reviews-artist">Music Fan</h2>
-                        <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
+            <div className="reviews-container">
+                <div className="reviews-speakers">
+                    <img src={Speaker1} alt="aa" className="reviews-speakers-img rsil"/>
+                    <img src={Speaker2} alt="aa" className="reviews-speakers-img rsir"/>
+                </div>
+                <div className="reviews-text">
+                    <h1 className="reviews-h">REVIEWS</h1>
+                    <div className="reviews-boxes">
+                        <div className="reviews-box">
+                            <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
+                            <h2 className="reviews-artist">Artist</h2>
+                            <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
+                        </div>
+                        <div className="reviews-box">
+                            <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
+                            <h2 className="reviews-artist">Producer</h2>
+                            <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
+                        </div>
+                        <div className="reviews-box">
+                            <div className="reviews-rating"><img src={Stars} alt="stars?" width="100%" /></div>
+                            <h2 className="reviews-artist">Music Fan</h2>
+                            <h3 className="reviews-testimonials">"Love it, it's the best. I can't live without it!"</h3>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        </>
     )
 }
 
